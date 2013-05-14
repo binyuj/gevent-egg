4f97134ded794780ff0d6de13d77197d05f012da
Phus Lu <phus.lu@gmail.com>
2013-05-14 10:09:00 +0800

    first commit

---
+++ gevent/ares.py
@@ -0,0 +1,7 @@
+def __bootstrap__():
+   global __bootstrap__, __loader__, __file__
+   import sys, pkg_resources, imp, platform
+   __file__ = pkg_resources.resource_filename(__name__,'ares-py%s-%s-%s%s' % (sys.version[:3],sys.platform,platform.machine(),imp.get_suffixes()[0][0]))
+   __loader__ = None; del __bootstrap__, __loader__
+   imp.load_dynamic(__name__,__file__)
+__bootstrap__()
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from distutils.core import setup
+
+if sys.version[:3] not in ('2.6', '2.7'):
+    raise NotImplementedError("Sorry, you need at Python 2.6/2.7 to use gevent-egg.")
+
+if sys.platform not in ('linux2', 'darwin', 'win32'):
+    raise NotImplementedError("Sorry, you need at Linux/MacOS/WinNT to use gevent-egg.")
+
+import gevent
+
+setup(name='gevent',
+      version=gevent.__version__,
+      description='Coroutine-based network library',
+      long_description=gevent.__doc__,
+      author='Denis Bilenko',
+      author_email='denis.bilenko@gmail.com',
+      url='http://gevent.org/',
+      packages=['gevent', 'greenlet'],
+      package_data={'gevent': ['*.so'], 'greenlet': ['*.so']},
+      #cmdclass=dict(build_ext=my_build_ext, sdist=sdist),
+      license='MIT',
+      platforms='any',
+      classifiers=["License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python :: 2.6",
+                   "Programming Language :: Python :: 2.7",
+                   "Operating System :: MacOS :: MacOS X",
+                   "Operating System :: POSIX",
+                   "Operating System :: Microsoft :: Windows",
+                   "Topic :: Internet",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   "Intended Audience :: Developers",
+                   "Development Status :: 4 - Beta"],)
